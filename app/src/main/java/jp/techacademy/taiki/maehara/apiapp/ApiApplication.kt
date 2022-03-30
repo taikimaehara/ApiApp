6531c6fea9da5a0f59ceeb9d63cb029cd9aca864
ta-maehara <ta-maehara@kddi-tech.com>
2022-03-30 20:31:27 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/taiki/maehara/apiapp/ApiApplication.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.taiki.maehara.apiapp
+
+import android.app.Application
+import io.realm.Realm
+
+class ApiApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
